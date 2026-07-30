1ffe1a2a5ec4bae57e3cc0f63d206d760c66acc1
Sharvari Pathade <sharvaripathade@gmail.com>
2026-07-30 22:15:22 +0530

    Solved HackerRank Day 1 Data Types

---
+++ Python/day 1(1).py
@@ -0,0 +1,14 @@
+# Declare constants
+i = 4
+d = 4.0
+s = "HackerRank "
+
+# Read input values
+integer_value = int(input())
+double_value = float(input())
+string_value = input()
+
+# Perform operations
+print(i + integer_value)
+print(d + double_value)
+print(s + string_value)
